d235add64823c77f67cb0bab501ba80fe775dc8a
Martin Unger <martin-unger2@live.at>
2025-06-02 19:44:57 +0200

    enhance CORS settings by adding allowed methods and origins

--- core/settings.py
+++ core/settings.py
@@ -64,12 +64,35 @@ MIDDLEWARE = [
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
+    'https://coderr.martin-unger.at'
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
     'https://coderr.martin-unger.at'
+]
+
+CORS_ALLOW_CREDENTIALS = True 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
 
 
